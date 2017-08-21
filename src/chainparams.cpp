af91a59060dadf4a5d3f1dd056a547c080dc1a7a
Mihai-George Cornateanu <mihai-george.cornateanu@borngroup.com>
2017-08-21 05:06:49 +0100

    buildnow3nd

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -120,37 +120,37 @@ public:
 
 
 
-/**
-*	if (true && genesis.GetHash() != consensus.hashGenesisBlock)
-*        {
-*		printf("Searching for genesis block...\n");
-*		// This will figure out a valid hash and Nonce if you're
-*		// creating a different genesis block:
-*		uint256 hashTarget = CBigNum().SetCompact(genesis.nBits).getuint256();
-*		uint256 thash;
-*		char scratchpad[SCRYPT_SCRATCHPAD_SIZE];
-*
-*		loop
-*		{
-*               	scrypt_1024_1_1_256_sp(BEGIN(genesis.nVersion), BEGIN(thash), scratchpad);
-*                	if (thash <= hashTarget)
-*                    		break;
-*                	if ((genesis.nNonce & 0xFFF) == 0)
-*                	{
-*                    		printf("nonce %08X: hash = %s (target = %s)\n", genesis.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
-*                	}
-*                	++genesis.nNonce;
-*                	if (genesis.nNonce == 0)
-*                	{
-*                    		printf("NONCE WRAPPED, incrementing time\n");
-*	                    	++genesis.nTime;
-*			}
-*		}
-*            	printf("genesis.nTime = %u \n", genesis.nTime);
-*            	printf("genesis.nNonce = %u \n", genesis.nNonce);
-*            	printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
-*	}
-*/
+
+	if (true && genesis.GetHash() != consensus.hashGenesisBlock)
+        {
+		printf("Searching for genesis block...\n");
+		// This will figure out a valid hash and Nonce if you're
+		// creating a different genesis block:
+		uint256 hashTarget = CBigNum().SetCompact(genesis.nBits).getuint256();
+		uint256 thash;
+		char scratchpad[SCRYPT_SCRATCHPAD_SIZE];
+
+		loop
+		{
+      	        	scrypt_1024_1_1_256_sp(BEGIN(genesis.nVersion), BEGIN(thash), scratchpad);
+                	if (thash <= hashTarget)
+                    		break;
+                	if ((genesis.nNonce & 0xFFF) == 0)
+                	{
+                    		printf("nonce %08X: hash = %s (target = %s)\n", genesis.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
+                	}
+                	++genesis.nNonce;
+                	if (genesis.nNonce == 0)
+                	{
+                    		printf("NONCE WRAPPED, incrementing time\n");
+	                    	++genesis.nTime;
+			}
+		}
+            	printf("genesis.nTime = %u \n", genesis.nTime);
+            	printf("genesis.nNonce = %u \n", genesis.nNonce);
+            	printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+	}
+
 
 
 
